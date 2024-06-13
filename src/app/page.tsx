040b8b65b7bb238f4dd10490de5b1d47ef180d5a
slince-zero <1711257704@qq.com>
2024-06-13 15:46:09 +0800

    完成布局

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div className="text-red-300 text-2xl">Hello, Next.js!sss</div>
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="min-w-48 min-h-full bg-blue-300">left-1</div>
+      <div className="min-w-64 min-h-full bg-blue-200">left-2</div>
+      <div className="flex flex-col flex-1 bg-blue-100">
+        <div className="flex w-full min-h-[50%]">
+          <div className="min-w-[80%]">top-1</div>
+          <div>top-2</div>
+        </div>
+        <div>bottom</div>
+      </div>
+    </div>
+  )
 }
