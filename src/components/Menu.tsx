78ed2636a1aa15fbcbb7dab485c92d9b601f77be
slince-zero <1711257704@qq.com>
2024-06-13 17:30:31 +0800

    修改一些细节

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,14 +3,13 @@
 import { useContext } from 'react'
 import { StoreContext } from '@/store'
 import { observer } from 'mobx-react-lite'
-
+import { AiOutlineVideoCameraAdd } from 'react-icons/ai'
+import Store from '@/store/store'
 const MENU_OPTIONS = [
   {
     name: 'Video',
-    icon: MdVideoLibrary,
-    action: (store: Store) => {
-      store.setSelectedMenuOption('Video')
-    },
+    icon: AiOutlineVideoCameraAdd,
+    action: (store: Store) => {},
   },
 ]
 
