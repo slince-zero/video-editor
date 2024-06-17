36262739292862e624bf9238b62ddba19c3d1a63
slince-zero <1711257704@qq.com>
2024-06-17 14:18:58 +0800

    细节修改

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,14 @@ import { MenuOption } from '@/type'
 class Store {
   selectedMenuOption: MenuOption
   images: string[]
+  videos: string[]
+  audios: string[]
+
   constructor() {
     this.selectedMenuOption = 'Video'
     this.images = []
+    this.videos = []
+    this.audios = []
     makeAutoObservable(this)
   }
 
@@ -15,10 +20,26 @@ class Store {
 
   /**
    * @description
-   * 添加图片到 images 数组里面
+   * 添加图片到 images 数组里面,用于 resource 模块的展示
    */
   addImageResource(image: string) {
     this.images = [...this.images, image]
+  }
+
+  /**
+   * @description
+   * 添加视频到 videos 数组里面,用于 resource 模块的展示
+   */
+  addVideoResource(video: string) {
+    this.videos = [...this.videos, video]
+  }
+
+  /**
+   * @description
+   * 添加音频到 audios 数组里面,用于 resource 模块的展示
+   */
+  addAudioResource(audio: string) {
+    this.audios = [...this.audios, audio]
   }
 }
 
