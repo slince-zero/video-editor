02ae272af5c83e52d1ba594d65ed6c74fb8853a3
slince-zero <1711257704@qq.com>
2024-06-17 10:17:51 +0800

    添加resource板

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Menu from '@/components/Menu'
+import Resource from '@/components/Resource'
 export default function Home() {
   return (
     <div className="flex h-screen w-screen">
       <div className="min-w-48 min-h-full bg-blue-300">
         <Menu />
       </div>
-      <div className="min-w-64 min-h-full bg-blue-200">left-2</div>
+      <div className="min-w-64 min-h-full bg-blue-200">
+        <Resource />
+      </div>
       <div className="flex flex-col flex-1 bg-blue-100">
         <div className="flex w-full min-h-[50%]">
           <div className="min-w-[80%]">top-1</div>
---
+++ src/components/Resource.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { StoreContext } from '@/store'
+import { observer } from 'mobx-react-lite'
+
+const Resource = observer(() => {
+  return <>11</>
+})
+
+export default Resource
