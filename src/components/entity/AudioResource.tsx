971768a3ba512286fc6c31a261f4fd8a12be32ad
slince-zero <1711257704@qq.com>
2024-06-17 15:27:07 +0800

    上传音乐+展示完成

---
+++ src/components/entity/AudioResource.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { StoreContext } from '@/store'
+import { observer } from 'mobx-react-lite'
+import { useContext, useRef, useState } from 'react'
+import { formatTimeToMinSec } from '../utils'
+
+type AudioResourceProps = {
+  audio: string
+  index: number
+}
+
+const AudioResource = observer(({ audio, index }: AudioResourceProps) => {
+  const store = useContext(StoreContext)
+  const ref = useRef<HTMLAudioElement>(null)
+  const [audioLength, setAudioLength] = useState('00:00')
+  return (
+    <div className="rounded-lg overflow-hidden items-center bg-slate-800 m-[15px] flex flex-col relative">
+      <div className=" text-white py-1 absolute text-base top-2 right-2">
+        {audioLength}
+      </div>
+      <button
+        className="hover:bg-[#00a0f5] bg-[rgba(0,0,0,.25)] rounded z-10 text-white font-bold py-1 absolute text-lg bottom-2 right-2"
+        onClick={() => console.log('添加到编辑界面')}
+      >
+        add
+      </button>
+      <div className='h-[100px]'>
+        <audio
+          onLoadedData={() => {
+            const audioLength = ref.current?.duration ?? 0
+            setAudioLength(formatTimeToMinSec(audioLength))
+          }}
+          ref={ref}
+          src={audio}
+          id={`audio-${index}`}
+        />
+      </div>
+    </div>
+  )
+})
+
+export default AudioResource
--- src/components/resourcePanels/AudioResourcePanel.tsx
+++ src/components/resourcePanels/AudioResourcePanel.tsx
@@ -3,9 +3,31 @@
 import { StoreContext } from '@/store'
 import { observer } from 'mobx-react-lite'
 import { useContext } from 'react'
+import AudioResource from '../entity/AudioResource'
+import SharedButton from '../utils/SharedButton'
+
 const AudioResourcePanel = observer(() => {
   const store = useContext(StoreContext)
-  return <div>audio</div>
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (!file) return
+    store.addAudioResource(URL.createObjectURL(file))
+  }
+  console.log(store.audios);
+  
+  return (
+    <div>
+      <div>image</div>
+      <SharedButton
+        accept="audio/mp3,audio/*"
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-center mx-2 py-2 px-4 rounded cursor-pointer"
+        onChange={handleFileChange}
+      />
+      {store.audios.map((audio, index) => {
+        return <AudioResource key={audio} audio={audio} index={index} />
+      })}
+    </div>
+  )
 })
 
 export default AudioResourcePanel
