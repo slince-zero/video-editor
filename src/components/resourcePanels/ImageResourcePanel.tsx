52fe5e11814f84e25116d2a106a0f0b84c077598
slince-zero <1711257704@qq.com>
2024-06-17 13:09:12 +0800

    Image-图片上传done

--- src/components/resourcePanels/ImageResourcePanel.tsx
+++ src/components/resourcePanels/ImageResourcePanel.tsx
@@ -2,10 +2,49 @@
 
 import { StoreContext } from '@/store'
 import { observer } from 'mobx-react-lite'
-import { useContext } from 'react'
+import React, { useContext } from 'react'
+
+type ImageChooseButton = {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  className?: string
+  accept: string
+}
+
+// button 组件
+const ImageChooseButton = (props: ImageChooseButton) => {
+  return (
+    <label htmlFor="fileInput" className={props.className}>
+      <input
+        id="fileInput"
+        type="file"
+        accept={props.accept}
+        className="hidden"
+        onChange={props.onChange}
+      />
+      Upload
+    </label>
+  )
+}
+
 const ImageResourcePanel = observer(() => {
   const store = useContext(StoreContext)
-  return <div>image</div>
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (!file) return
+    store.addImageResource(URL.createObjectURL(file))
+  }
+  console.log(store.images);
+  
+  return (
+    <div>
+      <div>image</div>
+      <ImageChooseButton
+        accept="image/*"
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-center mx-4 py-2 px-4 rounded cursor-pointer"
+        onChange={handleFileChange}
+      />
+    </div>
+  )
 })
 
 export default ImageResourcePanel
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,23 @@ import { makeAutoObservable } from 'mobx'
 import { MenuOption } from '@/type'
 class Store {
   selectedMenuOption: MenuOption
-
+  images: string[]
   constructor() {
     this.selectedMenuOption = 'Video'
+    this.images = []
     makeAutoObservable(this)
   }
 
   setSelectedMenuOption(option: MenuOption) {
     this.selectedMenuOption = option
+  }
+
+  /**
+   * @description
+   * 添加图片到 images 数组里面
+   */
+  addImageResource(image: string) {
+    this.images = [...this.images, image]
   }
 }
 
