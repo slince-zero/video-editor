de1787fdc12b313a9f490673d67bb79466663e23
slince-zero <1711257704@qq.com>
2024-06-24 10:15:36 +0800

    添加 MainBoard 组件，改变组件读取方式

---
+++ src/components/MainBoard.tsx
@@ -0,0 +1,26 @@
+import Menu from '@/components/Menu'
+import Resource from '@/components/Resource'
+import TimeLine from '@/components/TimeLine'
+const MainBoard = () => {
+  return (
+    <div className='flex h-screen w-screen'>
+      <div className='min-w-48 min-h-full bg-blue-300'>
+        <Menu />
+      </div>
+      <div className='min-w-64 min-h-full bg-blue-200'>
+        <Resource />
+      </div>
+      <div className='flex flex-col flex-1 bg-blue-100'>
+        <div className='flex w-full min-h-[50%]'>
+          <div className='min-w-[80%]'>top-1</div>
+          <div>top-2</div>
+        </div>
+        <div>
+          <TimeLine />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainBoard
